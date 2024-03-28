79791abbd8d11e11838d963d75d85da1f510a521
Prafulmhapsekar <prafulmhapsekar@gmail.com>
2024-03-28 21:55:38 +0530

    android project commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+import React from "react";
+import { Text, Image, ImageBackground, View, StyleSheet } from "react-native";
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.pageContainer}>
+      {/* <View style={styles.card}> */}
+      <ImageBackground source={require('./assets/rentabuddylogo/Imagelogo.png')}
+        style={styles.image}>
+
+      </ImageBackground>
+      {/* </View> */}
     </View>
-  );
-}
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  pageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+  // card: {
+  //   height: '35%',
+  //   width: '55%',
+  //   borderRadius: 10,
+  //   shadowColor: "#000",
+  //   shadowOffset: {
+  //     width: ,
+  //     height: 12,
+  //   },
+  //   shadowOpacity: 0.58,
+  //   shadowRadius: 16.00,
 
+  //   elevation: 24,
+  // },
+  image: {
+    height: '55%',
+    width: '75%',
+  
+    // borderRadius: 10,
+    // overflow: 'hidden',
+    // justifyContent: 'flex-end'
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 25,
+    color: 'white',
+
+  },
+  bio: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  cardInner: {
+    padding: 10
+  }
+})
 export default App;
